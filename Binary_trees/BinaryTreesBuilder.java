17818f0b0085f4ed2756a817dfe1126c5fc1b88b
adi1905 <adityathorat866@gmail.com>
2024-03-01 10:30:16 +0530

    Added child parent sum code!

--- Binary_trees/BinaryTreesBuilder.java
+++ Binary_trees/BinaryTreesBuilder.java
@@ -210,7 +210,24 @@ public class BinaryTreesBuilder {
 	    return false;
     }
 
-
+    //Function to check whether all nodes of a tree have the value 
+    //equal to the sum of their child nodes
+    public static int isSumProperty(Node root)
+    {
+        if(root == null){
+            return 1;
+        }
+        if(root.left == null && root.right == null){
+            return 1;
+        }
+        int leftSum = root.left != null ? root.left.data : 0;
+        int rightSum = root.right!=null ? root.right.data : 0;
+        
+        if(root.data == (leftSum + rightSum) && isSumProperty(root.left) == 1 && isSumProperty(root.right) == 1){
+            return 1;
+        }
+        return 0;
+    }
 
     public static void main(String[] args) {
         int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
