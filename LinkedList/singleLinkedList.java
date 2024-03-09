09f8e68350bd695f5a1098c9a92fad7df4e36799
adi1905 <adityathorat866@gmail.com>
2024-03-09 20:31:11 +0530

    Added swapPairs logic

--- LinkedList/singleLinkedList.java
+++ LinkedList/singleLinkedList.java
@@ -71,6 +71,20 @@ public class singleLinkedList {
         return head;
     }
 
+    public Node swapPairs(Node head) {
+        if(head == null){
+            return null;
+        }
+        Node temp = head;
+        while(temp!=null && temp.next!=null){
+            int k = temp.data;
+            temp.data = temp.next.data;
+            temp.next.data = k;
+            temp = temp.next.next;
+        }
+        return head;
+    }
+
     public Node doubleIt(Node head) {
         if(head.data >=5){
             Node temp = head;
@@ -88,10 +102,15 @@ public class singleLinkedList {
         }
         return head;
     }
+
+
     public static void main(String[] args) {
-        Node head = new Node(10);
-        head.next = new Node(16);
-        head.next.next = new Node(20);
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
         insertGcdNodes(head);
         printLinkedList(head);
         // int gcdNumber = gcd(4, 10);
