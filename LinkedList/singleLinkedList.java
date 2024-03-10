17555a831205744aa0fa65e343fe59f27f74e03e
adi1905 <adityathorat866@gmail.com>
2024-03-10 23:31:23 +0530

    Added mrore linked list questions

--- LinkedList/singleLinkedList.java
+++ LinkedList/singleLinkedList.java
@@ -1,5 +1,5 @@
 package LinkedList;
-
+import java.util.*;
 
 public class singleLinkedList {
     public static Node head;
@@ -84,7 +84,45 @@ public class singleLinkedList {
         }
         return head;
     }
+    public static Node reverseListBruteForce(Node head){
+        Stack<Integer> s = new Stack<>();
+        Node temp = head;
+        while(temp!=null){
+            s.push(temp.data);
+            temp = temp.next;
+        }
+        temp = head;
+        while(!s.isEmpty()){
+            int currentData = s.pop();
+            temp.data = currentData;
+            temp = temp.next;
+        }
+        return head;
+    }
+    public static Node reverseList3Pointer(Node head){
+        Node prev = null;
+        Node temp = head;
+        while(temp!=null){
+            Node nextNode = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = nextNode; 
+        }
+        return prev;
+    }
 
+    public static void pushAtEnd(Node head,int data){
+        if(head == null){
+            head = new Node(data);
+        }else{
+            Node newNode = new Node(data);
+            Node temp = head;
+            while(temp.next!=null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
     public Node doubleIt(Node head) {
         if(head.data >=5){
             Node temp = head;
@@ -106,13 +144,19 @@ public class singleLinkedList {
 
     public static void main(String[] args) {
         Node head = new Node(1);
-        head.next = new Node(2);
-        head.next.next = new Node(3);
-        head.next.next.next = new Node(4);
-        head.next.next.next.next = new Node(5);
+        
+        // head.next = new Node(2);
+        pushAtEnd(head,2);
+        pushAtEnd(head, 3);
+        // head.next.next = new Node(3);
+        // head.next.next.next = new Node(4);
+        // head.next.next.next.next = new Node(5);
 
-        insertGcdNodes(head);
+        // insertGcdNodes(head);
         printLinkedList(head);
+        System.out.println();
+        Node newHead = reverseList3Pointer(head);
+        printLinkedList(newHead);
         // int gcdNumber = gcd(4, 10);
 
         // System.out.println(gcdNumber);
